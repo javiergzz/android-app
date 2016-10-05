26f213cbad0dd8170bfec7137fea0d5923f77647
Javier Gonzalez <fcogzz.r@gmail.com>
2016-10-05 15:06:18 -0500

    Avances de Settings.

--- app/src/main/java/com/grahm/livepost/activities/SettingsActivity.java
+++ app/src/main/java/com/grahm/livepost/activities/SettingsActivity.java
@@ -2,17 +2,38 @@ package com.grahm.livepost.activities;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.grahm.livepost.R;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterSession;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static com.grahm.livepost.activities.SplashScreen.PREFS_LOGIN;
+import static com.grahm.livepost.activities.SplashScreen.PREFS_NAME;
 
 public class SettingsActivity extends AppCompatActivity{
+
     private static final String TAG = "SettingsActivity";
+    private static final int DISPLAY_LENGTH = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +46,50 @@ public class SettingsActivity extends AppCompatActivity{
         Uri uri = Uri.parse(v.getTag().toString());
         Intent intent = new Intent(Intent.ACTION_VIEW, uri);
         startActivity(intent);
+    }
+
+    @OnClick(R.id.btn_log_out)
+    public void logOut(){
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+
+        TwitterSession twitterSession = TwitterCore.getInstance().getSessionManager().getActiveSession();
+        if (twitterSession != null) {
+            Twitter.getSessionManager().clearActiveSession();
+            Twitter.logOut();
+        }
+
+        editor.putString("user", "");
+        editor.putString("uid", "");
+        editor.putString("username", "");
+        editor.putString(SplashScreen.PREFS_AUTH, "");
+        editor.putBoolean(SplashScreen.PREFS_LOGIN, false);
+        editor.commit();
+
+        FirebaseAuth.getInstance().signOut();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SettingsActivity.this, Login.class);
+                SettingsActivity.this.startActivity(mainIntent);
+                SettingsActivity.this.finish();
+            }
+        }, DISPLAY_LENGTH);
+    }
+
+    public void changePassword(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String newPassword = "SOME-SECURE-PASSWORD";
+        user.updatePassword(newPassword)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                        }
+                    }
+                });
+
     }
 }
 
